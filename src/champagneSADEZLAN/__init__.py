3a436a3d003585ec31b9d0fbc8888aa0603aa339
Sadezlan <axel.loschiavo@unil.ch>
2025-11-16 17:43:01 +0100

    Commit initial configuration of __init__.py file

---
+++ src/champagneSADEZLAN/__init__.py
@@ -0,0 +1,14 @@
+from .glass import GlassProfile
+from .radius_fun import f, f_vec_for_integrate, S
+# from .simulate import simulate_parties
+# from .weather import CityWeather, expected_lambda
+
+__all__ = [
+    "GlassProfile",
+    "f",
+    "f_vec_for_integrate",
+    "S",
+    "simulate_parties",
+    "CityWeather",
+    "expected_lambda",
+]
