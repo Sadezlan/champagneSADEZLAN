61b42039bc7e5cae2f903db097d54469973d04d0
Sadezlan <axel.loschiavo@unil.ch>
2025-11-16 23:37:42 +0100

    Imporving __init__.py with CityWeather dataclass

--- src/champagneSADEZLAN/__init__.py
+++ src/champagneSADEZLAN/__init__.py
@@ -1,7 +1,7 @@
 from .glass import GlassProfile
 from .radius_fun import f, f_vec_for_integrate, S
 # from .simulate import simulate_parties
-# from .weather import CityWeather, expected_lambda
+from .weather import CityWeather
 
 __all__ = [
     "GlassProfile",
